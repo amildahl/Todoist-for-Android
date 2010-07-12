07eff0906e8f5750229db1d6fe026f7556ce1b3e
Alex Brandt <alunduil@alunduil.com>
2010-07-12 09:01:49 +0800

    Still adding contentproviders for the objects.

--- src/com/drewdahl/android/todoist/projects/Todoist.java
+++ src/com/drewdahl/android/todoist/projects/Todoist.java
@@ -52,28 +52,11 @@ public final class Todoist {
          */
         public static final String DEFAULT_SORT_ORDER = "modified DESC";
 
-        /**
-         * The title of the note
-         * <P>Type: TEXT</P>
-         */
-        public static final String TITLE = "title";
-
-        /**
-         * The note itself
-         * <P>Type: TEXT</P>
-         */
+        public static final String NAME = "name";
         public static final String COLOR = "color";
-
-        /**
-         * The timestamp for when the note was created
-         * <P>Type: INTEGER (long from System.curentTimeMillis())</P>
-         */
-        public static final String CREATED_DATE = "created";
-
-        /**
-         * The timestamp for when the note was last modified
-         * <P>Type: INTEGER (long from System.curentTimeMillis())</P>
-         */
-        public static final String MODIFIED_DATE = "modified";
+        public static final String COLLAPSED = "collapsed";
+        public static final String ITEM_ORDER = "item_order";
+        public static final String CACHE_COUNT = "cache_count";
+        public static final String INDENT = "inent";
     }
 }
