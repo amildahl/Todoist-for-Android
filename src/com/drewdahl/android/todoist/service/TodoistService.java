f9ba175f69118a4c2edd843f3366fc51d708ce8a
Alex Brandt <alunduil@alunduil.com>
2010-08-09 01:36:03 +0800

    Adding a service that is mentioned.

---
+++ src/com/drewdahl/android/todoist/service/TodoistService.java
@@ -0,0 +1,19 @@
+package com.drewdahl.android.todoist.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TodoistService extends Service {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
