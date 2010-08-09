87676c4790dfeb8b7c56157d4a2bf1ee358a9bbb
Alex Brandt <alunduil@alunduil.com>
2010-08-09 09:32:56 +0800

    Adding more stuff to the service.

--- src/com/drewdahl/android/todoist/service/TodoistService.java
+++ src/com/drewdahl/android/todoist/service/TodoistService.java
@@ -1,5 +1,9 @@
 package com.drewdahl.android.todoist.service;
 
+import com.drewdahl.android.todoist.apihandler.TodoistApiHandler;
+import com.drewdahl.android.todoist.models.Item;
+import com.drewdahl.android.todoist.models.Project;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -8,6 +12,32 @@ public class TodoistService extends Service {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		startSync();
+	}
+	
+	class TodoistWorker implements Runnable {
+		public void run() {
+			/**
+			 * TODO Make this check the cache so it only grabs items to update that it should be.
+			 */
+			for (Project p : TodoistApiHandler.getInstance().getProjects()) {
+				p.save(TodoistService.this.getContentResolver());
+				for (Item i : TodoistApiHandler.getInstance().getUncompletedItems(p.getId())) {
+					i.save(TodoistService.this.getContentResolver());
+				}
+			}
+		}
+	}
+	
+	private void startSync() {
+		Thread child = new Thread(null, new TodoistWorker(), "TodoistService");
+		child.start();
+	}
+	
+	@Override
+	public void onStart(Intent intent, int startId) {
+		super.onStart(intent, startId);
+		startSync();
 	}
 	
 	@Override
@@ -15,5 +45,4 @@ public class TodoistService extends Service {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
