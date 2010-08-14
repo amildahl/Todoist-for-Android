8f182c3cf5faea17180b41d03d62991ac348a943
Alex Brandt <alunduil@alunduil.com>
2010-08-14 13:37:53 -0500

    Updated the models to work with the cursor.

--- src/com/drewdahl/android/todoist/service/TodoistService.java
+++ src/com/drewdahl/android/todoist/service/TodoistService.java
@@ -6,42 +6,75 @@ import java.util.concurrent.TimeUnit;
 import com.drewdahl.android.todoist.apihandler.TodoistApiHandler;
 import com.drewdahl.android.todoist.models.Item;
 import com.drewdahl.android.todoist.models.Project;
+import com.drewdahl.android.todoist.provider.TodoistProviderMetaData.Items;
+import com.drewdahl.android.todoist.provider.TodoistProviderMetaData.Projects;
 
 import android.app.Service;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.IBinder;
+import android.util.Log;
 
 public class TodoistService extends Service {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		startSync();
+		initialSync();
+		startScheduledSync();
 	}
 	
 	class TodoistWorker implements Runnable {
 		public void run() {
-			/**
-			 * TODO Make this check the cache so it only grabs items to update that it should be.
-			 */
 			if (TodoistApiHandler.getInstance().getUser() != null) {
-				for (Project p : TodoistApiHandler.getInstance().getProjects()) {
-					p.save(TodoistService.this.getContentResolver());
-					for (Item i : TodoistApiHandler.getInstance().getUncompletedItems(p.getId())) {
-						i.save(TodoistService.this.getContentResolver());
-					}
+				Long now = Long.valueOf(System.currentTimeMillis());
+
+				Cursor c = null;
+				
+				c = getContentResolver().query(Projects.CONTENT_URI, null, Projects.CACHE_TIME + "<=" + now.toString(), null, null);
+				for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+					Project p = new Project(c, TodoistApiHandler.getInstance().getUser());
+					p.save(getContentResolver());
+				}
+
+				c = getContentResolver().query(Items.CONTENT_URI, null, Items.CACHE_TIME + "<=" + now, null, null);
+				for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+					Item i = new Item(c, TodoistApiHandler.getInstance().getUser());
+					i.save(getContentResolver());
+				}
+			}
+			/**
+			 * TODO Threads need to die.
+			 */
+		}
+	}
+	
+	private void startScheduledSync() {
+		ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(1);
+		scheduler.scheduleAtFixedRate(new TodoistWorker(), 0, 3, TimeUnit.SECONDS);
+	}
+	
+	private void initialSync() {
+		if (TodoistApiHandler.getInstance().getUser() != null) {
+			Log.d(this.toString(), "Project Count: " + TodoistApiHandler.getInstance().getProjects().size());
+			for (Project p : TodoistApiHandler.getInstance().getProjects()) {
+				Log.d(this.toString(), "Adding project: " + p.getName());
+				p.save(TodoistService.this.getContentResolver());
+				for (Item i : TodoistApiHandler.getInstance().getUncompletedItems(p.getId())) {
+					Log.d(this.toString(), "Adding item: " + i.getId());
+					i.save(TodoistService.this.getContentResolver());
+				}
+				for (Item i : TodoistApiHandler.getInstance().getCompletedItems(p.getId())) {
+					Log.d(this.toString(), "Adding item: " + i.getId());
+					i.save(TodoistService.this.getContentResolver());
 				}
 			}
 		}
 	}
 	
-	private void startSync() {
-		ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(1);
-		scheduler.scheduleAtFixedRate(new TodoistWorker(), 0, 360, TimeUnit.SECONDS);
-	}
-	
 	@Override
 	public void onStart(Intent intent, int startId) {
 		super.onStart(intent, startId);
+		startScheduledSync();
 	}
 	
 	@Override
