8eb60019da0cae7e144d05f0c0b0c381e8dd4ab8
Alex Brandt <alunduil@alunduil.com>
2010-08-15 14:58:06 -0500

    Adding the components for adding items.`

--- src/com/drewdahl/android/todoist/views/ItemEdit.java
+++ src/com/drewdahl/android/todoist/views/ItemEdit.java
@@ -1,7 +1,15 @@
 package com.drewdahl.android.todoist.views;
 
+import com.drewdahl.android.todoist.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ItemEdit extends Activity {
-
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.itemedit);
+	}
 }
